047b1c3f65442903dca33ec5df88c80cebe0b5e8
kurotaku <kuroiwa.taku@gmail.com>
2022-01-12 00:07:46 +0900

    temp

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,20 +3,16 @@ module Api
     skip_before_action :verify_authenticity_token
 
     def create
-      cookies.permanent[:jwt] = { value: 'success', httponly: true, same_site: :none, secure: true }
-      render json: { success: { messages: ['test'] } }
-      p '========'
-      p cookies
-      # user = User.find_by(email: session_params[:email])
+      user = User.find_by(email: session_params[:email])
 
-      # if user&.authenticate(session_params[:password])
-      #   token = Jwt::TokenProviderService.call(user_id: user.id)
-      #   # cookies.signed.permanent[:jwt] = { value: token, httponly: true }
-      #   cookies.permanent[:jwt] = { value: token, httponly: true, same_site: :none, secure: true }
-      #   render json: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer).as_json.deep_merge(user: { token: token })
-      # else
-      #   render json: { error: { messages: ['mistake email or password'] } }, status: :unauthorized
-      # end
+      if user&.authenticate(session_params[:password])
+        token = Jwt::TokenProviderService.call(user_id: user.id)
+        # cookies.signed.permanent[:jwt] = { value: token, httponly: true }
+        cookies.permanent[:jwt] = { value: token, httponly: true, same_site: :none, secure: true }
+        render json: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer).as_json.deep_merge(user: { token: token })
+      else
+        render json: { error: { messages: ['mistake email or password'] } }, status: :unauthorized
+      end
     end
 
     def destroy
