c09a3767ac18e74386c6f944842359ead5ddf4b6
kurotaku <kuroiwa.taku@gmail.com>
2022-01-11 21:02:55 +0900

    temp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://0.0.0.0:3000', 'http://localhost:3000', 'http://a4e4-240d-1a-d29-ec00-6d94-90c1-85b5-8d51.ngrok.io', 'https://a4e4-240d-1a-d29-ec00-6d94-90c1-85b5-8d51.ngrok.io'
+    origins 'http://0.0.0.0:3000', 'http://localhost:3000', 'https://b121-240d-1a-d29-ec00-a9d1-7561-953b-202b.ngrok.io'
     resource '*',
       headers: :any,
       methods: :any,
